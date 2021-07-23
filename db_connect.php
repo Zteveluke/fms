b33c7d6d8f058fc979b0c5a4c210f95bcdc153d6
Steve Luke <52157482+Zteveluke@users.noreply.github.com>
2021-07-23 13:23:06 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','fms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('remotemysql.com','CJp6QMpPjw','deKkCwdora','CJp6QMpPjw')or die("Could not connect to mysql".mysqli_error($con));
